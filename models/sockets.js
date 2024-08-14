cd4fedb2fdad4b55aec67dc644075af711f91d03
AlexanderDeMatteo <100955676+AlexanderDeMatteo@users.noreply.github.com>
2024-08-14 14:35:50 +0000

    cambios

--- models/sockets.js
+++ models/sockets.js
@@ -14,11 +14,13 @@ class Sockets {
         this.io.on('connection', ( socket ) => {
 
             // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( ) => {
+            socket.on('mensaje-to-server', () => {
                 console.log( "conectado" );
                 
                 this.io.emit('mensaje-from-server', data );
             });
+            
+            socket.emit('mensaje-de-bienvenida', "bienvenido al server")
             
         
         });
